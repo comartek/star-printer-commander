ca4239bb04d6c391899aac43ac7b4d9af8496bbd
loidv-comartek <loidv.ovp@comartek.vn>
2023-06-20 10:00:12 +0700

    init package

--- src/actions/get-barcode.ts
+++ src/actions/get-barcode.ts
@@ -1,43 +1,44 @@
-import { spawn } from "child_process";
-import { StarCLIClient } from "../StarCLIClient";
-import { NewLineStream } from "../helpers/new-line-stream";
+import { spawn } from 'child_process';
+import { StarCLIClient } from '../StarCLIClient';
+import { NewLineStream } from '../helpers/new-line-stream';
 
 export function getBarcode(this: StarCLIClient, portName: string) {
-  return new Promise<{
-    barcodes: string[];
-  }>((resolve, reject) => {
-    let barcodes: string[] = [];
-    const theCommand = this.pathToCLI;
-    const args = ["barcode", "--port", portName];
+	return new Promise<{
+		barcodes: string[];
+	}>((resolve, reject) => {
+		let barcodes: string[] = [];
+		const theCommand = this.pathToCLI;
+		const args = ['barcode', '--port', portName];
 
-    const childProcess = spawn(theCommand, args);
+		const childProcess = spawn(theCommand, args);
 
-    const wsStdOut = new NewLineStream();
-    const wsStrErr = new NewLineStream();
+		const wsStdOut = new NewLineStream();
+		const wsStrErr = new NewLineStream();
 
-    childProcess.stdout.pipe(wsStdOut);
-    childProcess.stderr.pipe(wsStrErr);
+		childProcess.stdout.pipe(wsStdOut);
+		childProcess.stderr.pipe(wsStrErr);
 
-    wsStdOut.on("data", (buffer: Buffer) => {
-      const data = buffer.toString();
-      barcodes = data
-        .trim()
-        .replace(new RegExp("\rBarcode", "g"), "")
-        .split(":")
-        .splice(1)
-        .filter((i) => i !== "");
-    });
+		wsStdOut.on('data', (buffer: Buffer) => {
+			const data = buffer.toString();
+			barcodes = data
+				.trim()
+				// eslint-disable-next-line no-control-regex
+				.replace(new RegExp('\rBarcode', 'g'), '')
+				.split(':')
+				.splice(1)
+				.filter((i) => i !== '');
+		});
 
-    wsStrErr.on("data", (buffer: Buffer) => {
-      wsStdOut.destroy();
-      wsStrErr.destroy();
-      reject(new Error("STD Error: " + buffer.toString()));
-    });
+		wsStrErr.on('data', (buffer: Buffer) => {
+			wsStdOut.destroy();
+			wsStrErr.destroy();
+			reject(new Error('STD Error: ' + buffer.toString()));
+		});
 
-    childProcess.on("close", () => {
-      wsStdOut.destroy();
-      wsStrErr.destroy();
-      resolve({ barcodes });
-    });
-  });
+		childProcess.on('close', () => {
+			wsStdOut.destroy();
+			wsStrErr.destroy();
+			resolve({ barcodes });
+		});
+	});
 }
